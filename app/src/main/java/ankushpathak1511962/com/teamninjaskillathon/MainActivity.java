a69f6eb88a62f16fa43205efeed8f8777bba9f67
Chandan Singh <sc231997@gmail.com>
2017-08-29 23:57:06 +0530

    ListView onItemClickListner added to show detailed info. about the registration

--- app/src/main/java/ankushpathak1511962/com/teamninjaskillathon/MainActivity.java
+++ app/src/main/java/ankushpathak1511962/com/teamninjaskillathon/MainActivity.java
@@ -1,6 +1,8 @@
 package ankushpathak1511962.com.teamninjaskillathon;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +11,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -16,6 +20,8 @@ import android.widget.TextView;
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 import ankushpathak1511962.com.teamninjaskillathon.R;
 
@@ -27,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     ListView listView;
     DatabaseReference databaseReference;
     FirebaseListAdapter listAdapter;
+    ArrayList <Entry>detailArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,8 +57,15 @@ public class MainActivity extends AppCompatActivity {
             protected void populateView(View v, Entry model, int position) {
                 ((TextView)v.findViewById(android.R.id.text1)).setText(model.getName());
                 ((TextView)v.findViewById(android.R.id.text2)).setText(model.getSerialNo());
+                detailArrayList.add(model);
             }
         };
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                showDetailsDialog(position);
+            }
+        });
         listView.setAdapter(listAdapter);
     }
 
@@ -59,8 +73,28 @@ public class MainActivity extends AppCompatActivity {
         //view initialization
         fab = (FloatingActionButton) findViewById(R.id.fab);
         listView = (ListView)findViewById(R.id.listView);
+        detailArrayList=new ArrayList<Entry>();
         //firebase reference
         databaseReference = FirebaseDatabase.getInstance().getReference().child("TeamNinja");
+    }
+    void showDetailsDialog(int position){
+        final Dialog dialog=new Dialog(MainActivity.this,android.R.style.Theme_DeviceDefault_Light_Dialog);
+        dialog.setContentView(R.layout.dialog_detail);
+        dialog.setTitle("Registration detail");
+
+        ((TextView)dialog.findViewById(R.id.textViewName)).append(detailArrayList.get(position).getName());
+        ((TextView)dialog.findViewById(R.id.textViewSrNo)).append(detailArrayList.get(position).getSerialNo());
+        ((TextView)dialog.findViewById(R.id.textViewEmail)).append(detailArrayList.get(position).getEmail());
+        ((TextView)dialog.findViewById(R.id.textViewPhone)).append(detailArrayList.get(position).getPhoneNo());
+        Button buttonOk=(Button)dialog.findViewById(R.id.buttonOk);
+
+        buttonOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
